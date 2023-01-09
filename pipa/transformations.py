0637f54b980d3a3bd8b1fa732d339c9824d678ea
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-01-09 13:24:27 -0800

    🔥✔️ Remove example test from template

--- pipa/transformations.py
+++ pipa/transformations.py
@@ -1,9 +1,5 @@
 import os
 
 
-def add_offset(augend: int, addend: int) -> int:
-    return augend + addend
-
-
 def repo_init() -> None:
     os.system("template in GitHub")
--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -1,16 +1,8 @@
 import os
-import pipa as dt
-
-
-def test_add_offset():
-    augend = 1
-    addend = 2
-    expected = augend + addend
-    obtained = dt.add_offset(augend, addend)
-    assert expected == obtained
+import pipa as pp
 
 
 def test_repo_init(mocker):
     mocker.patch("os.system")
-    dt.repo_init()
+    pp.repo_init()
     os.system.assert_called_once_with("template in GitHub")
