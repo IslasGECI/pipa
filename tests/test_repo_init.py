da3d1b0fdfc09bb072465475a0534b7d701698af
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-01-09 13:40:19 -0800

    🛑🧪 Test `module_name_in_files`

--- tests/test_repo_init.py
+++ tests/test_repo_init.py
@@ -6,3 +6,9 @@ def test_repo_init(mocker):
     mocker.patch("os.system")
     pp.repo_init()
     os.system.assert_called_once_with("template in GitHub")
+
+
+def test_modele_name_in_files(mocker):
+    mocker.patch("os.system")
+    pp.change_module_name_in_makefile()
+    os.system.assert_called_once_with("sed --in-place 's/dummy_transformations/pipa/' Makefile")
