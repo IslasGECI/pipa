2a159fadf38da03f908ea022b8b8f1417ce1ae87
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-01-09 13:52:23 -0800

    ♻️🔥 Remove repeated command

--- src/repo_init.sh
+++ src/repo_init.sh
@@ -1,4 +1,3 @@
 mv --force dummy_transformation/ pipa
-sed --in-place 's/dummy_transformations/pipa/' pyproject.toml
 sed --in-place 's/dummy_transformations/pipa/' tests/test_transformations.py
 sed --in-place 's/dummy_transformations/pipa/' .github/workflows/develop.yml
