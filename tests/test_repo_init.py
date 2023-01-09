7159cdd4ef3b8cc735f5609c9ce5f039cbf35ee8
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-01-09 13:46:27 -0800

    🛑🧪 Test `module_name_in_files`

--- tests/test_repo_init.py
+++ tests/test_repo_init.py
@@ -12,3 +12,5 @@ def test_modele_name_in_files(mocker):
     mocker.patch("os.system")
     pp.change_module_name_in_makefile()
     os.system.assert_called_once_with("sed --in-place 's/dummy_transformations/pipa/' Makefile")
+    pp.change_module_name_in_pyproject()
+    os.system.assert_called_once_with("sed --in-place 's/dummy_transformations/pipa/' pyproject.toml")
