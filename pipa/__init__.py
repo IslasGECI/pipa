ffbe1cf19a23839cb8e852543c217aaefc5c57b2
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-01-09 12:47:34 -0800

    ✔️ Pass test

--- pipa/__init__.py
+++ pipa/__init__.py
@@ -1,4 +1,4 @@
 """A template Python module"""
 
-__version__ = "0.4.0"
-from .transformations import add_offset  # noqa
+__version__ = "0.1.0"
+from .transformations import *  # noqa
--- pipa/transformations.py
+++ pipa/transformations.py
@@ -1,2 +1,7 @@
+import os
+
 def add_offset(augend: int, addend: int) -> int:
     return augend + addend
+
+def repo_init() -> None:
+    os.system("template in GitHub")
