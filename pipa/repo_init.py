0bbccb10dcb28ed36f510ccb4aa30d150f0035c6
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-01-09 13:44:07 -0800

    ✔️ Pass test

--- pipa/repo_init.py
+++ pipa/repo_init.py
@@ -3,3 +3,7 @@ import os
 
 def repo_init() -> None:
     os.system("template in GitHub")
+
+
+def change_module_name_in_makefile() -> None:
+    os.system("sed --in-place 's/dummy_transformations/pipa/' Makefile")
